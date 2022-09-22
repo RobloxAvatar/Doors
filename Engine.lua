05e6304fc04048d3ecc2d85a850056ed168d009c
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2022-09-22 18:47:03 +0200

    Update Engine.lua

--- Engine.lua
+++ Engine.lua
@@ -2,7 +2,7 @@ local Doors = {}
 
 function Doors.MakeNotification(txt, time)
     local p = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxAvatar/Doors/main/Notify.lua"))()
-	  p:MakeNotification({Name = "JailWare", Content = txt, Time = time})
+	  p:MakeNotification({Name = "Warning", Content = txt, Time = time})
 end
 
 return Doors
